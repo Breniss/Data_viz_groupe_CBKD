1780001607fa5e2abb5a89b553161ad87bcbf66c
constancebld <constancebeillard@gmail.com>
2024-11-10 18:15:33 +0100

    Mise à jour du fichier de visualisation de données

--- Data-visualisation/Data_viz_groupe_CBKD.R
+++ Data-visualisation/Data_viz_groupe_CBKD.R
@@ -1,53 +1,74 @@
-# Projet
-#This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
+#Projet Data-visualisation sur les séismes à forte magnétude à l'ouest des Etats-Unis
 
 library(ggplot2)
 library(shiny)
+library(leaflet)
 
-# Define UI for application that draws a histogram
+#Etape 1 : découverte des données 
+
+# Charger les données
+data <- read.csv("data1.csv")
+
+# Afficher un aperçu des données
+head(data)
+
+# Résumé statistique des données
+summary(data)
+
+# Structure des données
+str(data)
+
+#Etape 2 : Passage sur R Shiny
+
+# Interface utilisateur
+library(shiny)
+library(leaflet)
+
+# UI : Interface utilisateur avec deux onglets
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Application sur les Séismes aux États-Unis"),
+  
+  # Utilisation de tabsetPanel pour les fenêtres multiples
+  tabsetPanel(
+    tabPanel("Carte",
+             sidebarLayout(
+               sidebarPanel(
+                 sliderInput("radius", "Taille des points", min = 1, max = 10, value = 3),
+                 helpText("Ajustez la taille des points pour une meilleure visibilité.")
+               ),
+               mainPanel(
+                 leafletOutput("map", height = 600)
+               )
+             )
+    ),
+    tabPanel("Résumé des données",
+             mainPanel(
+               tableOutput("summary")
+             )
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+# Serveur : logique de l'application
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  # Carte des séismes
+  output$map <- renderLeaflet({
+    leaflet(data = data) %>%
+      addTiles() %>%
+      addCircleMarkers(~longitude, ~latitude,
+                       radius = ~input$radius,
+                       color = "red",
+                       stroke = FALSE,
+                       fillOpacity = 0.6,
+                       popup = ~paste("Magnitude:", mag))
+  })
+  
+  # Résumé des données
+  output$summary <- renderTable({
+    summary(data)  # Résumé statistique des données
+  })
 }
 
-# Run the application 
+# Lancement de l'application
 shinyApp(ui = ui, server = server)
